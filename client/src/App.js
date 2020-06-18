d9ed1773d443f7311c04fe73625986ee87495fcd
ffcabbar <ffcabbar@gmail.com>
2020-06-18 18:50:59 +0300

    Context Provider and checkLoggedIn added

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,59 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Home from "./components/Pages/Home";
 import Login from "./components/Auth/Login";
 import Register from "./components/Auth/Register";
 import Header from "./components/Layout/Header";
+import UserContext from "./context/UserContext";
+import Axios from "axios";
 
 const App = () => {
+  const [userData, setUserData] = useState({
+    token: undefined,
+    user: undefined,
+  });
+
+  useEffect(() => {
+    checkLoggedIn();
+  }, []);
+
+  const checkLoggedIn = async () => {
+    let token = localStorage.getItem("auth-token");
+
+    if (token === null) {
+      localStorage.setItem("auth-token", "");
+      token = "";
+    }
+
+    const tokenRes = await Axios.post(
+      "http://localhost:5000/users/tokenIsValid",
+      null,
+      { headers: { "x-auth-token": token } }
+    );
+
+    if (tokenRes.data) {
+      const userRes = await Axios.get("http://localhost:5000/users/", {
+        headers: { "x-auth-token": token },
+      });
+
+      setUserData({
+        token,
+        user: userRes.data,
+      });
+    }
+  };
+
   return (
     <>
       <BrowserRouter>
-        <Header />
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/login" component={Login} />
-          <Route path="/register" component={Register} />
-        </Switch>
+        <UserContext.Provider value={{ userData, setUserData }}>
+          <Header />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/login" component={Login} />
+            <Route path="/register" component={Register} />
+          </Switch>
+        </UserContext.Provider>
       </BrowserRouter>
     </>
   );
